f8297ab837532ecf3981509b184fd00fd00ff945
Nic <nicholas.vickery-wilson@paduafinancialgroup.com.au>
2020-05-28 21:21:09 +1000

    add-albums-carousel: Reduce query page size.

--- src/lambda/lastfm-query-handler.js
+++ src/lambda/lastfm-query-handler.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 export async function handler(event, context) {
     try {
-        const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=eps11&period=1month&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json`;
+        const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=eps11&period=1month&api_key=${process.env.REACT_APP_LAST_FM_API_KEY}&format=json&limit=25`;
         const response = await axios.get(url, {
             headers: { Accept: 'application/json' },
         });
